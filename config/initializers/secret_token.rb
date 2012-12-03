70df47b217b26bf658ca6870c1de06df9e53119f
Robert Ferney <capnregex@gmail.com>
2012-12-02 19:42:20 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ImageArchiveMg::Application.config.secret_token = '9957b9b0b952d2d0726878bb66db1478c29df4f69f1e2cc8c3708cba45b69cbdc1f4eba261e48f6eda614e2e871575e9fa9f46688268ce97c7871af331503ef0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ImageArchiveMg::Application.config.session_store :cookie_store, key: '_ImageArchiveMg_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ImageArchiveMg::Application.config.session_store :active_record_store
